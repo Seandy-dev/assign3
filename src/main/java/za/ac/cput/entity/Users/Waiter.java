b10e7a8ddd63dfa43eda813488ef640d280f5c80
Thina <Thina>
2021-06-01 22:27:56 +0200

    First commit Milestone 1st June 2021

--- src/main/java/za/ac/cput/entity/Users/Waiter.java
+++ src/main/java/za/ac/cput/entity/Users/Waiter.java
@@ -1,4 +1,68 @@
 package za.ac.cput.entity.Users;
 
+/*
+ * Waiter.java
+ * Entity for Waiter.
+ * Thina Mbiza - 217217095
+ * Date: 01 June 2021
+ */
 public class Waiter {
+
+    private String name, surname;
+    private String employeeNumber;
+    private String salary;
+
+    private Waiter(Builder builder) {
+        this.name = builder.name;
+        this.surname = builder.surname;
+        this.employeeNumber = builder.employeeNumber;
+        this.salary = builder.salary;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Waiter{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", employeeNumber=" + employeeNumber +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public static class Builder {
+        private String name, surname, employeeNumber, salary;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder setEmployeeNumber(String employeeNumber) {
+            this.employeeNumber = employeeNumber;
+            return this;
+        }
+
+        public Builder setSalary(String salary) {
+            this.salary = salary;
+            return this;
+        }
+
+        public Waiter build() {
+            return new Waiter(this);
+        }
+    }
+
+    public Waiter copy(Waiter waiter) {
+        this.name = waiter.name;
+        this.surname = waiter.surname;
+        this.employeeNumber = waiter.employeeNumber;
+        this.salary = waiter.salary;
+        return this;
+    }
 }
--- src/main/java/za/ac/cput/entity/coffeeshop/Menu.java
+++ src/main/java/za/ac/cput/entity/coffeeshop/Menu.java
@@ -1,4 +1,68 @@
 package za.ac.cput.entity.coffeeshop;
 
+/*
+ * Menu.java
+ * Entity for Menu.
+ * Thina Mbiza - 217217095
+ * Date: 01 June 2021
+ */
+
 public class Menu {
+    
+    private String breakfast, lunch, tea, coffee;
+
+    private Menu(Builder builder){
+        this.breakfast = builder.breakfast;
+        this.lunch = builder.lunch;
+        this.tea = builder.tea;
+        this.coffee = builder.coffee;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "breakfast='" + breakfast + '\'' +
+                ", lunch='" + lunch + '\'' +
+                ", tea='" + tea + '\'' +
+                ", coffee='" + coffee + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private String breakfast, lunch, tea, coffee;
+
+        public Builder setBreakfast(String breakfast) {
+            this.breakfast = breakfast;
+            return this;
+        }
+
+        public Builder setLunch(String lunch) {
+            this.lunch = lunch;
+            return this;
+        }
+
+        public Builder setTea(String tea) {
+            this.tea = tea;
+            return this;
+        }
+
+        public Builder setCoffee(String coffee) {
+            this.coffee = coffee;
+            return this;
+        }
+
+        public Builder copy(Menu menu){
+            this.breakfast = menu.breakfast;
+            this.lunch = menu.lunch;
+            this.coffee = menu.coffee;
+            this.tea = menu.tea;
+            return this;
+        }
+        public Menu build(){
+            return new Menu(this);
+        }
+    }
 }
+
+
