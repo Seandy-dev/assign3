e68afe84e55c30ae423cbdec0fc8a7035cbbfcc3
Nikitha Mbokotwana <lonambokotwan117@gmail.com>
2021-06-01 20:10:31 +0200

    First Commit Code

--- src/main/java/za/ac/cput/entity/coffeeshop/Shop.java
+++ src/main/java/za/ac/cput/entity/coffeeshop/Shop.java
@@ -1,4 +1,42 @@
 package za.ac.cput.entity.coffeeshop;
-
+/* Customer.java
+ Entity for the Shop
+ Author: Nikitha Mbokotwana (218337906)
+ Date: 01 June 2021
+*/
 public class Shop {
+    private String name;
+
+    private Shop(Builder builder) {
+        this.name = builder.name;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String name;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder copy(Shop shop) {
+            this.name = shop.name;
+
+
+            return this;
+        }
+
+        public Shop build() {
+            return new Shop(this);
+
+        }
+
+    }
 }
